f96804b0ab0bc75b2cfe0b69c03cd89efa522698
Rafael Carvalho <rafacien.rc@gmail.com>
2022-12-02 09:03:15 -0400

    Lendo turbidez

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ n_atualizar=5    #Quantidade de amostras antes de atualizar as variáveis
 janela=4         #Tamnho da janela da matriz
 k = 3            #Número de vizinhos no KNN
 
-#Inicializando a conexão MQTT 
+#Inicializando a conexão MQTT
 conexao = MQTT()
 conexao.setup()
 
 #Inicializando o intervalo
-intervalo = Intervalo()
+intervalo_temp = Intervalo()
+intervalo_turb = Intervalo()
 
 #Conexão com a porta serial
 try:
@@ -25,60 +26,90 @@ except:
 
 #Leitura da séria histórica e inicialização dos parâmetros
 try:
-    serie = fopen()
-    mat, l = gerar_matriz(serie, w=janela)
-    intervalo.update(l)
+    serie_temp = fopen(path="./SH_temperaturas")
+    mat_temp, l_temp = gerar_matriz(serie_temp, w=janela)
+    intervalo_temp.update(l_temp)
+    serie_turb = fopen(path="./SH_turbidez")
+    mat_turb, l_turb = gerar_matriz(serie_turb, w=janela)
+    intervalo_turb.update(l_turb)
 except: 
     #Não há série histórica, deverá ser construída
     print("Sem dados para Iniciar o modelo.")
     print(f"Serão feitas {max_entradas} leituras para iniciar o modelo.")
     n_entradas=0
-    entradas = []
+    entradas_temp = []
+    entradas_turb = []
     while n_entradas<max_entradas:
         line = serialPort.readline().decode().strip()
         if '*' in line:
             print(line)
-            dado = float(line.split("*")[1])
-            print("Nova Leitura:", dado)
-            conexao.publish(msg=f"Nova Leitura: {dado}")
-            entradas.append(dado)
+            temp = float(line.split("*")[1])
+            turb = float(line.split("*")[2])
+            print("Nova Leitura de Temperatura:", temp)
+            conexao.publish(msg=f"Nova Leitura de Temperatura: {temp}")
+            entradas_temp.append(temp)
+            print("Nova Leitura de Turbidez:", turb)
+            conexao.publish(msg=f"Nova Leitura de Turbidez: {turb}")
+            entradas_turb.append(turb)
             n_entradas+=1
-    create_csv(entradas)
+    create_csv(entradas_temp, path="./SH_temperaturas")
+    create_csv(entradas_turb, path="./SH_turbidez")
     print("Arquivo de entrada inicializado.")
-    serie = fopen()
-    mat, l = gerar_matriz(serie, w=janela)
-    intervalo.update(l)
+    serie_temp = fopen(path="./SH_temperaturas")
+    mat_temp, l_temp = gerar_matriz(serie_temp, w=janela)
+    intervalo_temp.update(l_temp)
+    serie_turb = fopen(path="./SH_turbidez")
+    mat_turb, l_turb = gerar_matriz(serie_turb, w=janela)
+    intervalo_turb.update(l_turb)
 
 #Variáveis de controle
 atualizar = 0
-ultima_janela = serie[-1*janela::]
+ultima_janela = serie_temp[-1*janela::]
 
 #Loop de execução
 while True:
     line = serialPort.readline().decode().strip()                            #Leitura do serial
     if '*' in line:                                                          #Se a linha contém dados
         print(line)
-        dado = float(line.split("*")[1])                                     #Separar o dado
-        print("Nova Leitura:", dado)
-        IP = intervalo.get_margem()                                          #Definir Intervalo de Predição
-        predicao = KNN_predict(ultima_janela, mat, l, k=k, size=len(l))      #Predição
-        print(f"Intervalo de predição: [{predicao-IP},{predicao+IP}]")
-        if dado >= predicao-IP and dado <= predicao+IP:
-            print("Leitura dentro do intervalo.")
-            update(dado)
-            conexao.publish(msg=f"Nova Leitura: {dado}")                    #Publicar dado
+        temp = float(line.split("*")[1])
+        turb = float(line.split("*")[2])
+        print("Nova Leitura de Temperatura:", temp)
+        IP = intervalo_temp.get_margem()                                          #Definir Intervalo de Predição
+        predicao = KNN_predict(ultima_janela, mat_temp, l_temp, k=k, size=len(l_turb))      #Predição
+        print(f"Intervalo de predição de Temperatura: [{predicao-IP},{predicao+IP}]")
+        if temp >= predicao-IP and temp <= predicao+IP:
+            print("Leitura de Temperatura dentro do intervalo.")
+            update(temp, path="./SH_temperaturas", len_max=max_entradas)
+            conexao.publish(msg=f"Nova Leitura de Temperatura: {temp}")                    #Publicar dado
             atualizar += 1
-            ultima_janela = ultima_janela[1::]+[dado]
+            ultima_janela = ultima_janela[1::]+[temp]
         else:
-            print("Leitura fora do intervalo.")
-            conexao.publish(msg=f"Nova Leitura Possivelmente Alterada: {dado}") #Publicar dado possivelmente alterado
+            print("Leitura de Temperatura fora do intervalo.")
+            conexao.publish(msg=f"Nova Leitura de Temperatura Possivelmente Alterada: {temp}") #Publicar dado possivelmente alterado
+        
+        print("Nova Leitura de Turbidez:", turb)
+        IP = intervalo_turb.get_margem()                                          #Definir Intervalo de Predição
+        predicao = KNN_predict(ultima_janela, mat_turb, l_turb, k=k, size=len(l_turb))      #Predição
+        print(f"Intervalo de predição de Turbidez: [{predicao-IP},{predicao+IP}]")
+        if turb >= predicao-IP and turb <= predicao+IP:
+            print("Leitura de Turbidez dentro do intervalo.")
+            update(turb, path="./SH_turbidez", len_max=max_entradas)
+            conexao.publish(msg=f"Nova Leitura de Turbidez: {turb}")                    #Publicar dado
+            atualizar += 1
+            ultima_janela = ultima_janela[1::]+[turb]
+        else:
+            print("Leitura de Turbidez fora do intervalo.")
+            conexao.publish(msg=f"Nova Leitura de Turbidez Possivelmente Alterada: {turb}") #Publicar dado possivelmente alterado
     
     if atualizar == n_atualizar:                                            #Atualizando variáveis internas
         print("Atualizando bancos de dados")
         atualizar=0
-        serie = fopen()
-        mat, l = gerar_matriz(serie, w=janela)
-        intervalo.update(l)
+        serie_temp = fopen(path="./SH_temperaturas")
+        mat_temp, l_temp = gerar_matriz(serie_temp, w=janela)
+        intervalo_temp.update(l_temp)
+        serie_turb = fopen(path="./SH_turbidez")
+        mat_turb, l_turb = gerar_matriz(serie_turb, w=janela)
+        intervalo_turb.update(l_turb)
 
 
 
